5cdb8b3cd4924e4002da226a4924daddc945a9cb
trunglk16 <trungnguyenlk16@gmail.com>
2019-01-17 11:27:56 +0700

    First

---
+++ src/main/java/com/codegym/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.codegym;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("123456").roles("USER")
+                .and()
+                .withUser("admin").password("12345").roles("ADMIN");
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/user**").hasRole("USER")
+                .and()
+                .authorizeRequests().antMatchers("/admin**").hasRole("ADMIN")
+                .and()
+                .formLogin()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    }
+}
